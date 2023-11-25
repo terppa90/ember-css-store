cbbc348e1bc1b0e30d45cdb54aa4871891587667
terppa90 <teromese@hotmail.com>
2023-11-25 14:42:34 +0200

    first commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'css-clothing-store/app';
+import config from 'css-clothing-store/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
